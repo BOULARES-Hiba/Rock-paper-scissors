3f522edf45faabf40a356caad21332bbd5f50ce5
BOULARES-Hibs <hibablr.info@gmail.com>
2024-07-05 18:31:51 +0100

    js-code

---
+++ game.js
@@ -0,0 +1,60 @@
+let score =  JSON.parse(localStorage.getItem('score'))  || {
+    wins : 0,
+    losses : 0,
+    ties :0
+};
+function game(playerChoice){
+    const computerMove = pickComputerMove();
+    document.querySelector('.computerC').innerHTML = `computer :${computerMove} ,you :${playerChoice}`;
+    if (computerMove === playerChoice) {
+        document.querySelector('.Result').innerHTML = 'Tie';
+        score.ties++;
+    } 
+    else if (
+        (playerChoice === 'rock' && computerMove === 'scissor') ||
+        (playerChoice === 'paper' && computerMove === 'rock') ||
+        (playerChoice === 'scissor' && computerMove === 'paper'))
+  {
+       document.querySelector('.Result').innerHTML = 'You Win';
+        score.wins++;
+    } 
+    else {
+        document.querySelector('.Result').innerHTML = 'You Lose';
+        score.losses++;
+    }
+    display();
+    localStorage.setItem('score',JSON.stringify(score));
+
+
+}
+
+function display(){
+document.querySelector('.scoreBoard')
+.innerHTML = `wins : ${score.wins} , losses : ${score.losses} ,ties :${score.ties}`;
+}
+
+ function pickComputerMove() {
+const randomNumber = Math.random();
+
+let computerMove = '';
+
+if (randomNumber >= 0 && randomNumber < 1 / 3) {
+  computerMove = 'rock';
+} else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+  computerMove = 'paper';
+} else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+  computerMove = 'scissor';
+}
+
+return computerMove;
+}
+function reset(){
+score.wins =0;
+score.losses=0;
+score.ties =0;
+
+display();
+
+
+localStorage.setItem('score',JSON.stringify(score));
+}
